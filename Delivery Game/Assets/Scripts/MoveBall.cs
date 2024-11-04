8ba21af9aecbafe45e35fd2c85dbe28da0742613
BrunoSS80 <BrunoSSilva751@gmail.com>
2024-11-04 18:59:30 -0300

    Finalizado a movimentação da bola ao clicar e arrastar.

---
+++ Delivery Game/Assets/Scripts/MoveBall.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MoveBall : MonoBehaviour
+{
+    public Rigidbody2D rb_Ball;
+    public float force = 4;
+    public Transform inicialForce, moveForce, maxForce;
+    public Vector3 direction, scanPos, screenPoint, offset;
+    void Start()
+    {
+        rb_Ball = GetComponent<Rigidbody2D>();
+    }
+    private void Update() {
+    }
+    
+    private void OnMouseDown() {
+        scanPos = moveForce.transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        offset = Camera.main.ScreenToWorldPoint(maxForce.transform.position);
+    }
+
+    private void OnMouseDrag() {
+        screenPoint = scanPos + Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        moveForce.transform.position = screenPoint;
+        Vector3 force = screenPoint - offset;
+        
+        if(moveForce.transform.position.y > maxForce.transform.position.y){
+            direction = moveForce.transform.position;
+        }
+        else if(moveForce.transform.position.y <= maxForce.transform.position.y){
+            direction.y = maxForce.transform.position.y;
+        }
+    }
+
+    private void OnMouseUp() {
+        rb_Ball.AddForce(-direction * force, ForceMode2D.Impulse);
+        direction = Vector3.zero;
+        moveForce.transform.position = transform.position;
+    }
+}
