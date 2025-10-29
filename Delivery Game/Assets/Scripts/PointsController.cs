0bd2814b17d93a54808f646512b6b504a98a05db
BrunoSS80 <BrunoSSilva751@gmail.com>
2025-10-29 20:24:50 -0300

    Feito script para mostrar o valor e quantidade de cada objeto na barra de criação e adicionado suas sprites na barra.

--- Delivery Game/Assets/Scripts/PointsController.cs
+++ Delivery Game/Assets/Scripts/PointsController.cs
@@ -1,39 +1,56 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PointsController : MonoBehaviour
 {
     public float valFan, valSlime;
+    [SerializeField]
+    private TextMeshProUGUI textSlimeValue, textSlimeQtd,textFanValue, textFanQtd;
     public Image imgBar, star1, star2, star3;
     [Header("Value for get Stars")]
     public float valStar3, valStar2, valStar1;
     public Animator star1Anim, star2Anim, star3Anim;
     public int starsWoned;
+    public SpawnController spawnController;
+    public int qtdSlime,qtdFan;
 
+    private void Awake() {
+        textSlimeValue.SetText($"{valSlime}");
+        textFanValue.SetText($"{valFan}");
+        qtdFan = spawnController.fanSpawn.Length;
+        qtdSlime = spawnController.slimeSpawn.Length;
+    }
     void Update()
     {
         CalcStars();
+        textSlimeQtd.SetText($"{qtdSlime}");
+        textFanQtd.SetText($"{qtdFan}");
     }
 
     public void AddFan()
     {
         float percentFan = valFan / 100;
         imgBar.fillAmount -= percentFan;
+        qtdFan -= 1;
     }
     public void AddSlime()
     {
         float percentSlime = valSlime / 100;
         imgBar.fillAmount -= percentSlime;
+        qtdSlime -= 1;
     }
     public void RemoveObj()
     {
         if (GameManager.Instance.moveObjectTag.CompareTag("Slime"))
         {
-            imgBar.fillAmount += (valSlime/100);
+            imgBar.fillAmount += (valSlime / 100);
+            qtdSlime += 1;
         }
         else
         {
-            imgBar.fillAmount += (valFan/100);
+            imgBar.fillAmount += (valFan / 100);
+            qtdFan += 1;
         }
         GameManager.Instance.moveObjectTag.SetActive(false);
         GameManager.Instance.moveObjectTag = null;
